8465f3eb01b58c4950c578ed5058e67acf9a7894
Antoine Drouault <antoinedrouault@hotmail.com>
2023-07-31 23:44:12 +0200

    Add retry button

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const itemsElements = document.querySelectorAll(".items");
+const fightElement = document.querySelector("#fight");
 const playerElement = document.querySelector("#player");
 const enemyElement = document.querySelector("#enemy");
 const resultElement = document.querySelector("#result");
+const retryButton = document.querySelector("#retry");
 
 const items = ["rock", "paper", "scissors"];
 
@@ -11,7 +13,16 @@ itemsElements.forEach((e) =>
   })
 );
 
+retryButton.addEventListener("click", function () {
+  fightElement.classList.remove("show");
+  resultElement.classList.remove("show");
+  retryButton.classList.remove("show");
+});
+
 function itemChosen(playerItem) {
+  fightElement.classList.add("show");
+  resultElement.classList.add("show");
+  retryButton.classList.add("show");
   const enemyItem = getRandomItem();
   playerElement.textContent = playerItem;
   enemyElement.textContent = enemyItem;
